0d204100e357721f0dd79c8838eb3a24aad7f1ab
01527111 <01527111@student.tuwien.ac.at>
2019-03-25 02:51:33 +0100

    read init file in settings

--- src/Settings.cpp
+++ src/Settings.cpp
@@ -1,4 +1,60 @@
 #include "Settings.h"
+#include "INIReader.h"
+#include <glm/detail/func_trigonometric.inl>
 
 Settings::Settings()
 = default;
+
+void Settings::Load()
+{
+	// init reader for ini files
+	INIReader reader("assets/settings.ini");
+
+	windowWidth = reader.GetInteger("window", "width", 800);
+	windowHeight = reader.GetInteger("window", "height", 800);
+	isFullscreen = reader.GetBoolean("window", "fullscreen", false);
+	title = reader.Get("window", "title", "Snowform").c_str();
+	maxFps = reader.GetInteger("window", "refresh_rate", 60);
+	
+	cameraFov = static_cast<float>(glm::radians(reader.GetReal("camera", "fov", 60.f)));
+	cameraNear = static_cast<float>(reader.GetReal("camera", "near", 0.1f));
+	cameraFar = static_cast<float>(reader.GetReal("camera", "far", 100.1f));
+}
+
+int Settings::getWindowWidth() const
+{
+	return windowWidth;
+}
+
+int Settings::getWindowHeight() const
+{
+	return windowHeight;
+}
+
+bool Settings::getFullscreen() const
+{
+	return isFullscreen;
+}
+
+const char* Settings::getTitle() const
+{
+	return title;
+}
+
+int Settings::getMaxFps() const
+{
+	return maxFps;
+}
+
+float Settings::getCameraFov() const
+{
+	return cameraFov;
+}
+float Settings::getCameraNear() const
+{
+	return cameraNear;
+}
+float Settings::getCameraFar() const
+{
+	return cameraFar;
+}
--- src/Settings.h
+++ src/Settings.h
@@ -9,9 +9,34 @@ public:
 		return instance;
 	}
 
+	// We want to make sure these two methods are unacceptable,
+   // otherwise we may accidentally get copies of our singleton
+	Settings(Settings const&) = delete;
+	void operator=(Settings const&) = delete;
+
+	void Load();
+	int getWindowWidth() const;
+	int getWindowHeight() const;
+	bool getFullscreen() const;
+	const char* getTitle() const;
+	int getMaxFps() const;
+
+	float getCameraFov() const;
+	float getCameraNear() const;
+	float getCameraFar() const;
+
 private:
 	Settings();
 
+	int windowWidth;
+	int windowHeight;
+	bool isFullscreen;
+	const char* title;
+	int maxFps;
+
+	float cameraFov;
+	float cameraNear;
+	float cameraFar;
 };
 
 #define sSettings Settings::getInstance()
