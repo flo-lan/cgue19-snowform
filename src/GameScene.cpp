900491982cd9524bc037ac792590e438ab41a1d5
Exxenoz <exxenoz@gmx.at>
2019-05-04 22:04:40 +0200

    Removed unused includes from GameScene

--- src/GameScene.cpp
+++ src/GameScene.cpp
@@ -1,16 +1,4 @@
 #include "GameScene.h"
-#include "GameObject.h"
-#include "TransformComponent.h"
-#include "ArcBallControllerComponent.h"
-#include "CameraComponent.h"
-#include "DirectionalLightComponent.h"
-#include "PointLightComponent.h"
-#include "SimpleMaterial.h"
-#include "AssetManager.h"
-#include "StandardMaterial.h"
-#include "MeshRendererComponent.h"
-#include "Settings.h"
-#include "Mesh.h"
 
 GameScene::GameScene() :
     Scene::Scene("Game Scene")
