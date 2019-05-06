d75fc5dbbd61ab25c0f50dbdf949e7c3aa425260
Exxenoz <exxenoz@gmx.at>
2019-05-06 19:10:42 +0200

    Added placeholder texts for coin count, FPS and remaining time

--- src/GlobalAssetLoader.cpp
+++ src/GlobalAssetLoader.cpp
@@ -63,6 +63,21 @@ bool GlobalAssetLoader::LoadAssets()
     auto snowballMaterial = sAssetManager.CreateMaterial<StandardMaterial>("Snowball", standardShaderProgram);
     snowballMaterial->SetDiffuseTexture(sAssetManager.GetTexture2D("snowball_diffuse"));
 
+    if (TextMaterial* tm = sAssetManager.CreateMaterial<TextMaterial>("CoinCount", textShaderProgram))
+    {
+        tm->SetAtlasTexture(sAssetManager.GetTexture2D("arial_atlas"));
+    }
+
+    if (TextMaterial* tm = sAssetManager.CreateMaterial<TextMaterial>("FPS", textShaderProgram))
+    {
+        tm->SetAtlasTexture(sAssetManager.GetTexture2D("arial_atlas"));
+    }
+
+    if (TextMaterial* tm = sAssetManager.CreateMaterial<TextMaterial>("RemainingTime", textShaderProgram))
+    {
+        tm->SetAtlasTexture(sAssetManager.GetTexture2D("arial_atlas"));
+    }
+
     sAssetManager.CreateQuadMesh("Image", 1.f, 1.f);
     sAssetManager.CreateCubeMesh("Cube", 1.5f, 1.5f, 1.5f);
     sAssetManager.CreateCylinderMesh("Cylinder", 32, 1.f, 1.3f);
