887105c7781131181baf81232e46a86a597a1c5c
Exxenoz <exxenoz@gmx.at>
2019-06-03 01:43:46 +0200

    Some camera component getters for transform, projection and view matrix

--- src/CameraComponent.cpp
+++ src/CameraComponent.cpp
@@ -8,6 +8,8 @@
 CameraComponent::CameraComponent(GameObject* owner) :
     Component::Component(owner),
     transform(GetOwner()->GetComponent<TransformComponent>()),
+    projMatrix(1.f),
+    viewMatrix(1.f),
     viewProjectionMatrix(1.f),
     orthographicProjectionMatrix(glm::ortho(0.0f, (float)Screen::GetWidth(), 0.0f, (float)Screen::GetHeight())),
     fov(60.f),
@@ -28,13 +30,13 @@ CameraComponent::~CameraComponent()
 
 void CameraComponent::LateUpdate()
 {
-    glm::mat4 projMatrix = glm::perspective(fov, aspectRatio, nearPlane, farPlane);
-    glm::mat4 viewMatrix = glm::inverse(transform->GetModelMatrix());
+    projMatrix = glm::perspective(fov, aspectRatio, nearPlane, farPlane);
+    viewMatrix = glm::inverse(transform->GetModelMatrix());
 
     viewProjectionMatrix = projMatrix * viewMatrix;
 }
 
 glm::vec3 CameraComponent::GetPosition() const
 {
-    return transform ? transform->GetPosition() : glm::vec3(0.f);
+    return transform->GetPosition();
 }
--- src/CameraComponent.h
+++ src/CameraComponent.h
@@ -19,12 +19,17 @@ public:
     void SetNearPlane(float value) { nearPlane = value; }
     void SetFarPlane(float value) { farPlane = value; }
 
+    TransformComponent* GetTransform() const { return transform; }
     glm::vec3 GetPosition() const;
+    glm::mat4 const& GetProjMatrix() const { return projMatrix; }
+    glm::mat4 const& GetViewMatrix() const { return viewMatrix; }
     glm::mat4 const& GetViewProjectionMatrix() const { return viewProjectionMatrix; }
     glm::mat4 const& GetOrthographicProjectionMatrix() const { return orthographicProjectionMatrix; }
 
 private:
     TransformComponent* transform;
+    glm::mat4 projMatrix;
+    glm::mat4 viewMatrix;
     glm::mat4 viewProjectionMatrix;
     glm::mat4 orthographicProjectionMatrix;
     float fov;
